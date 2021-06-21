b1419014b6c0e96f2bd1f9912201a4029ab0c098
Fredpery77 <fredpery77@msn.com>
2021-06-21 16:11:56 +0200

    commit initial

--- models/password.js
+++ models/password.js
@@ -3,12 +3,12 @@ const passwordValidator = require('password-validator');
 // création d'un schéma pour le mot de passe, pour renforcer la sécurité
 const passwordSchema = new passwordValidator();
 
-// ajout de propriétés que le mot de passe foit respecter
+// ajout de propriétés que le mot de passe doit respecter
 passwordSchema
-.is().min(8)                                            // Minimum length 8
-.has().uppercase()                                      // Must have uppercase letters
-.has().lowercase()                                      // Must have lowercase letters
-.has().digits()                                         // Must have digits
+.is().min(8)                                            // minimum 8 caratères
+.has().uppercase()                                      // doit contenir une lettre majuscule
+.has().lowercase()                                      // doit contenir une lettre minuscule
+.has().digits()                                         // doit contenir un chiffre
 .is().not().oneOf(['Passw0rd', 'Password123']);         // Blacklist these values
 
 module.exports = passwordSchema;
